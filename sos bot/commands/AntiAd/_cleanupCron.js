096aa082957ce165713d77d9be8b97de43116284
dhyeyppatel <dhyeyp254@gmail.com>
2026-07-28 04:14:49 +0530

    Move anti-ad modifications to root

--- sos bot/commands/AntiAd/_cleanupCron.js
+++
@@ -1,9 +0,0 @@
-let lastBotMsgId = Bot.getProperty("lastBotMessageId_" + chat.chatid);
-if (!lastBotMsgId) { return; }
-
-Api.sendMessage({
-  on_result: "/saveLastBotMessage",
-    chat_id: chat.chatid,
-    text: ".", 
-    on_result: "/onCheckerDelivered"
-});
--- sos bot/commands/AntiAd/_saveLastBotMessage.js
+++
@@ -1,4 +0,0 @@
-if (!options || !options.result) { return; }
-
-let messageId = options.result.message_id;
-Bot.setProperty("lastBotMessageId_" + chat.chatid, messageId, "integer");
